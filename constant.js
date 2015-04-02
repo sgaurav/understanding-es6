97d2b426e7e6fee3316ba8a8edc65b54da5720a8
sgaurav <sgaurav.baghel@gmail.com>
2015-04-02 14:47:08 +0530

    examples for let and constant

---
+++ constant.js
@@ -0,0 +1,10 @@
+/**
+  * constant is a single assignmet variable.
+  */
+
+function foo(){
+    const i = 1;
+    
+    var bar = i; // 1
+    var i = 2;  // error
+}
---
+++ let.js
@@ -0,0 +1,13 @@
+/**
+  * let desclares a block scope local variable. let has been in Firefox for a 
+  * long time and is now a part of ES6.
+  * IMPORTANT: If outside any block, let is scoped globally else will be scoped
+  * to nearest enclosing block.
+  */
+
+function foo(){
+  for(let i = 0; i< 10; i++){
+      // i is visible
+  }
+  // i is not visible
+}
