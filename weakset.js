84ace2baad1255dac2dc93b7a540b79ad0b90961
sgaurav <sgaurav.baghel@gmail.com>
2015-04-10 18:49:53 +0530

    added weakset example

---
+++ weakset.js
@@ -0,0 +1,20 @@
+/**
+  * WeakSet are collection of objects. It allows to store weakly held objects 
+  * in collection. Weakly help objects are unique across collection.
+  * Unlike Set, WeakSet can only store Objects. Objects are held weakly and no
+  * reference is stored and hence prevents leaking.
+  */
+
+var x = {};
+var y = function(){};
+
+var ws = new WeakSet();
+
+ws.add(x);
+ws.add(y);
+
+ws.has(x); // true
+
+ws.delete(x);
+
+ws.clear(); // purge whole weakset
