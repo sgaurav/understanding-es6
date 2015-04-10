b52e9d0e65b3a6dd6377d70f7606581480dc5080
sgaurav <sgaurav.baghel@gmail.com>
2015-04-10 18:41:55 +0530

    added set

---
+++ set.js
@@ -0,0 +1,16 @@
+/**
+  * Set is a collection of unique values. A Set can be iterated in order of 
+  * insertion of its elements.
+  */
+
+var foo = new Set();
+
+foo.add(1);
+foo.add(2);
+foo.add("three");
+
+foo.has(1); // true
+
+foo.size(); // 3
+
+foo.delete(2); // removes 2 from foo
