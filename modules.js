1846eaa6261fda56daaab40fc77770098072e798
Gaurav Singh <sgaurav.baghel@gmail.com>
2016-01-26 13:44:36 +0530

    added modules

---
+++ modules.js
@@ -0,0 +1,12 @@
+/**
+  * extract data from arrays or objects
+  */
+
+// utils.js
+export const pi = 3.14;
+export function add(x, y){
+    return x + y;
+}
+
+// index.js
+import {pi, add} from utils;
