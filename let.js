0ed1d77011a0c2e03ebf912af8e6eaefcac9d9b8
Phillip Ventura <phiven@gmail.com>
2016-01-28 11:05:31 -0800

    Update let.js

--- let.js
+++ let.js
@@ -1,7 +1,7 @@
 /**
   * let declares a block scope local variable. let has been in Firefox for a 
   * long time and is now a part of ES6.
-  * IMPORTANT: If outside any block, let is scoped globally else will be scoped
+  * IMPORTANT: If outside of any block, let is scoped globally else will be scoped
   * to nearest enclosing block.
   */
 
