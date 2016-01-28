2f2618d0af6cecb23af5edb8e88d6cb83aeb683a
gregan <gregegan8@gmail.com>
2016-01-28 10:45:56 -0800

    Update template-string.js

--- template-string.js
+++ template-string.js
@@ -5,4 +5,20 @@
 var x = "foo";
 var y = "bar";
 
+// es5
+var join = x + ' ' + y; // foo bar
+
+// es6
 var join = `${x} ${y}`; // foo bar
+
+/*
+foo
+bar
+*/
+
+// es5
+var join = x + '\n' + y;
+
+// es6
+var join = `${x}
+${y}`;
