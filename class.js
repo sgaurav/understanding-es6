aa6a8b25e292b77189fb559576fd47d47e3f272c
Pat Laughlin <voipme2@gmail.com>
2016-01-28 14:21:14 -0500

    Added in ES5 version of the Animal class

--- class.js
+++ class.js
@@ -15,3 +15,19 @@ class Animal {
 var simone = new Animal("cat", "meow");
 
 simone.makeNoise(); // returns "The cat goes meow"
+
+/**
+ * ES5 version
+ */
+function ES5Animal(type, sound) {
+ this.type = type;
+ this.sound = sound;
+}
+
+ES5Animal.prototype.makeNoise = function() {
+ return 'The ' + this.type + ' goes ' + this.sound;
+}
+
+var dogbert = new ES5Animal("dog", "bark");
+
+dogbert.makeNoise();  // returns "The dog goes bark"
