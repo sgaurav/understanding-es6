5f3964882f02a4f1496ced5066edf6f236b18da1
volv <steven@bigham.com>
2016-01-28 20:24:12 +0000

    Destructuring examples

--- destructuring-assignment.js
+++ destructuring-assignment.js
@@ -2,9 +2,20 @@
   * extract data from arrays or objects
   */
 
-var foo = [1, 2, 3];
-var [one, two, three] = foo;
-// one => 1, two => 2, three => 3
+let foo = [1, 2, 3];
+let [one, two, three] = foo;
+// one = 1, two = 2, three = 3
 
-var {a, b} = {a:1, b:2};
-// a => 1, b => 2
+// Works in Functions!
+let myFunc = ([x, y, z]) => {
+  // x = 1, y = 2, z = 3
+}
+myFunc(foo);
+
+//Names need to match property names in case of object
+let {a, b} = {a:1, b:2};
+// a = 1, b = 2
+
+//Allows variable swapping with no temp variable
+[a, b] = [b, a];
+// a = 2, b = 1
