c0bf3a96e3a3fee32d53bb4ee66a5419389b91ff
Phillip Ventura <phiven@gmail.com>
2016-01-28 11:06:54 -0800

    Update maps.js

--- maps.js
+++ maps.js
@@ -2,8 +2,8 @@
   * Map object are simple key/value maps. Both object and primitive values can 
   * be used as key or value in map.
   * Although Map looks similar to Object, its worth noting that keys of Object  
-  * are string while their is no such restriction for a Map. Also Map size can 
-  * be retrieved like array while this is not the case with Objects.
+  * are strings while there is no such restriction for a Map. Also Map size can 
+  * be retrieved like an array while this is not the case with Objects.
   */
 
 var x = {};
