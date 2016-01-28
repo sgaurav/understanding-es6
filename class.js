bd31e197bfc9b4bf532262a2584419807a71d288
Josh Bivens <hotbioform@gmail.com>
2016-01-28 09:19:08 -0700

    Add simple class example

--- class.js
+++ class.js
@@ -1,5 +1,17 @@
 /**
   * Class is a syntactical sugar over prototype based inheritance.
   */
+class Animal { 
+  constructor(type, sound) {
+    this.type = type;
+    this.sound = sound;
+  }
+  
+  makeNoise() {
+    return ('The' + this.type + ' goes ' + this.sound);
+  }
+}
 
-  //TODO -- Write a simple example for classes
+var Simone = new Animal("cat", "meow");
+
+Simone.makeNoise(); // returns "The cat goes meow"
